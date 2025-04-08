fd84658661bc87babb5d8f104c938c2b2b1acfd2
programadorce <programadorceara@gmail.com>
2025-04-08 01:46:58 -0300

    Configurando o Style Guide no Tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-background-primary text-background-secondary">
       Hello World
     </div>
   );
