6bd007ebf9775c3cafc3d12106d2afe92380bc62
programadorce <programadorceara@gmail.com>
2025-04-09 00:49:52 -0300

    Início Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Hero from "./components/landing-page/Hero";
 
 export default function Home() {
   return (
-    <div className="bg-background-primary text-background-secondary">
-      Hello World
+    <div className="max-w-7xl mx-auto">
+      <Hero />
     </div>
   );
 }
