f19cd273ebd7ece846c275117cf44d8baccaa746
programadorce <programadorceara@gmail.com>
2025-04-16 23:41:33 -0300

    Página Criar Link

---
+++ app/(pages)/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Hero from "../components/landing-page/Hero";
+import Header from "../components/landing-page/Header";
+import VideoExplanation from "../components/landing-page/VideoExplanation";
+import Pricing from "../components/landing-page/Pricing";
+import Faq from "../components/landing-page/Faq";
+
+export default function Home() {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Header />
+      <Hero />
+      <VideoExplanation />
+      <Pricing />
+      <Faq />
+    </div>
+  );
+}
